db9984d012dd4bca4956d0995a8eb99db570f25c
Gabriel <cummingsg@protonmail.ch>
2021-10-13 10:44:46 -0400

    Specify DOM bindings for images and Buttons

--- cross-section.js
+++ cross-section.js
@@ -1,5 +1,14 @@
+/* button DOM bindings */
+
 let buttonOne = document.getElementById('one');
 let buttonTwo = document.getElementById('two');
 let buttonThree = document.getElementById('three');
 
+/* image DOM bindings */
+let overview = document.getElementById('main')
+let internal = document.getElementById('internal')
+let surface = document.getElementById('surface')
 
+
+
+buttonTwo
