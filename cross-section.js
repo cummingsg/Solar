066b8fdf39a510096c7ff33b2ce2c5e21d37d2cd
Gabriel <cummingsg@protonmail.ch>
2021-10-13 14:51:13 -0400

    Add rough functions for planet cross-sections

--- cross-section.js
+++ cross-section.js
@@ -1,14 +1,54 @@
 /* button DOM bindings */
 
-let buttonOne = document.getElementById('one');
-let buttonTwo = document.getElementById('two');
-let buttonThree = document.getElementById('three');
+const buttonOne = document.getElementById('one');
+const buttonTwo = document.getElementById('two');
+const buttonThree = document.getElementById('three');
 
 /* image DOM bindings */
-let overview = document.getElementById('main')
-let internal = document.getElementById('internal')
-let surface = document.getElementById('surface')
 
 
 
-buttonTwo
+function toggleOverview() {
+    const overview = document.getElementById('overview')
+    const internal = document.getElementById('internal')
+    const surface = document.getElementById('surface')
+    let displaySetting = overview.style.display;
+    if (displaySetting == 'block') {
+      overview.style.display = 'none';
+    }
+    else {
+      overview.style.display = 'block';
+
+    }
+  }
+
+  function toggleInternal() {
+    const overview = document.getElementById('overview')
+    const internal = document.getElementById('internal')
+    const surface = document.getElementById('surface')
+    let displaySetting = internal.style.display;
+    if (displaySetting == 'block') {
+      internal.style.display = 'none';
+    }
+    else {
+      internal.style.display = 'block';
+
+    }
+  }
+
+  function toggleSurface() {
+    const overview = document.getElementById('overview') 
+    const internal = document.getElementById('internal')
+    const surface = document.getElementById('surface')
+    let displaySetting = surface.style.display;
+    if (displaySetting == 'block') {
+      surface.style.display = 'none';
+    }
+    else {
+      surface.style.display = 'block';
+
+    }
+  }
+  buttonOne.onclick = toggleOverview;
+  buttonTwo.onclick = toggleInternal;
+  buttonThree.onclick = toggleSurface;
