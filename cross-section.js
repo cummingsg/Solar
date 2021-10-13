d0d1654f46c80ff991b55a69268f194624d04334
Gabriel <cummingsg@protonmail.ch>
2021-10-13 15:22:46 -0400

    completed cross-section image functions

--- cross-section.js
+++ cross-section.js
@@ -7,18 +7,18 @@ const buttonThree = document.getElementById('three');
 /* image DOM bindings */
 
 
-
 function toggleOverview() {
     const overview = document.getElementById('overview')
     const internal = document.getElementById('internal')
     const surface = document.getElementById('surface')
     let displaySetting = overview.style.display;
     if (displaySetting == 'block') {
-      overview.style.display = 'none';
+      overview.style.display = 'none'
     }
     else {
       overview.style.display = 'block';
-
+      internal.style.display = 'none';
+      surface.style.display = 'none';
     }
   }
 
@@ -31,8 +31,10 @@ function toggleOverview() {
       internal.style.display = 'none';
     }
     else {
-      internal.style.display = 'block';
 
+      internal.style.display = 'block';
+      overview.style.display = 'none';
+      surface.style.display = 'none';
     }
   }
 
@@ -46,7 +48,8 @@ function toggleOverview() {
     }
     else {
       surface.style.display = 'block';
-
+      overview.style.display = 'none';
+      internal.style.display = 'none';
     }
   }
   buttonOne.onclick = toggleOverview;
