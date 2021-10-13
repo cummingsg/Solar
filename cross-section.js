afd384f82a8fb98c76408754aebe32cb9b144fc7
Gabriel <cummingsg@protonmail.ch>
2021-10-13 15:54:53 -0400

    finish adding text functions, complete JS script

--- cross-section.js
+++ cross-section.js
@@ -5,51 +5,67 @@ const buttonTwo = document.getElementById('two');
 const buttonThree = document.getElementById('three');
 
 /* image DOM bindings */
+const overview = document.getElementById('overview');
+const internal = document.getElementById('internal');
+const surface = document.getElementById('surface');
 
+/*paragraph DOM bindings*/
+
+const outline = document.getElementById('outline');
+const interior = document.getElementById('interior');
+const geology = document.getElementById('geology');
 
 function toggleOverview() {
-    const overview = document.getElementById('overview')
-    const internal = document.getElementById('internal')
-    const surface = document.getElementById('surface')
+    
     let displaySetting = overview.style.display;
     if (displaySetting == 'block') {
       overview.style.display = 'none'
+      outline.style.display = 'none'
     }
     else {
       overview.style.display = 'block';
       internal.style.display = 'none';
       surface.style.display = 'none';
+
+      outline.style.display = 'block';
+      interior.style.display = 'none';
+      geology.style.display = 'none';
     }
   }
 
   function toggleInternal() {
-    const overview = document.getElementById('overview')
-    const internal = document.getElementById('internal')
-    const surface = document.getElementById('surface')
+    
     let displaySetting = internal.style.display;
     if (displaySetting == 'block') {
       internal.style.display = 'none';
+      interior.style.display = 'none'
     }
     else {
 
       internal.style.display = 'block';
       overview.style.display = 'none';
       surface.style.display = 'none';
+
+      outline.style.display = 'none';
+      interior.style.display = 'block';
+      geology.style.display = 'none';
     }
   }
 
   function toggleSurface() {
-    const overview = document.getElementById('overview') 
-    const internal = document.getElementById('internal')
-    const surface = document.getElementById('surface')
+    
     let displaySetting = surface.style.display;
     if (displaySetting == 'block') {
       surface.style.display = 'none';
     }
     else {
       surface.style.display = 'block';
-      overview.style.display = 'none';
+      overview.style.display = 'block';
       internal.style.display = 'none';
+
+      outline.style.display = 'none';
+      interior.style.display = 'none';
+      geology.style.display = 'block';
     }
   }
   buttonOne.onclick = toggleOverview;
